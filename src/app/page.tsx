a896ee77a024ab94af75888e4d3271c12cba10b7
Boris Damianov <borisvd4@gmail.com>
2023-08-29 10:41:12 +0300

    created reusable button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
-import Image from 'next/image'
+import Button from "./components/ui/Button";
 
 export default function Home() {
-  return (
-    <div className='text-red-500'>Hello world</div>
-  )
+  return <Button variant="ghost" size="lg">Hello World</Button>;
 }
